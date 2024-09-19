3dea339272d7b0d8d1ac1f657373cb7e867254ca
tonynguyen <tonyjsfreelancer@gmail.com>
2024-09-19 23:51:37 +0700

    a:

---
+++ strapi-project-t/src/api/products/controllers/products.ts
@@ -0,0 +1,7 @@
+/**
+ * products controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::products.products');
---
+++ strapi-project-t/src/api/products/routes/products.ts
@@ -0,0 +1,7 @@
+/**
+ * products router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::products.products');
